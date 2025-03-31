91da430b430a3e427bd5d793eb4f040833994eba
allurimeenakshi <meena.alluri2004@gmail.com>
2025-03-31 16:24:45 +0530

    improvements

--- client/src/RootLayout.jsx
+++ client/src/RootLayout.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
-import Header from './components/header/header.jsx'
-import Footer from './components/footer/footer.jsx';
+import Header from './components/header/Header.jsx'
+import Footer from './components/footer/Footer.jsx';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
